5e9c392f94c9ef753c5866278beba61ebcfbd72a
IvanH-85 <ivanhansen1985@hotmail.com>
2026-09-07 19:59:08 +0200

    Bump Espen preview script version

--- header.js
+++ header.js
@@ -23,7 +23,7 @@
       await loadScript('espen-login-greeting.js?v=20260907');
       await loadScript('workout-names.js?v=20260907b');
       await loadScript('next-workout.js?v=20260907');
-      await loadScript('espen-preview.js?v=20260907');
+      await loadScript('espen-preview.js?v=20260907b');
     }catch(e){
       console.error('Kunne ikke laste GainTrain-moduler',e);
     }
