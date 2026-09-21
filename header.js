56707bfa34475a0e84d10493f1456142494e8e69
IvanH-85 <ivanhansen1985@hotmail.com>
2026-09-21 10:00:16 +0200

    Activate Action Mode v2 after GainTrain modules

--- header.js
+++ header.js
@@ -28,6 +28,7 @@
       await loadScript('challenge-status-history.js?v=20260914');
       await loadScript('gaintrain-fun.js?v=20260921');
       await loadScript('straffekassa.js?v=20260921');
+      await loadScript('action-theme-v2.js?v=20260921-v2a');
     }catch(e){
       console.error('Kunne ikke laste GainTrain-moduler',e);
     }
